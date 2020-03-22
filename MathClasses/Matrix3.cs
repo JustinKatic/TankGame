073f8ffd9df0e2cfbd0ba9c05cf1ec6510fa6ae8
Katic44 <justinkatic4@gmail.com>
2020-03-22 12:37:49 +1100

    added multiplcation matrix 3 and matrix 4

--- MathClasses/Matrix3.cs
+++ MathClasses/Matrix3.cs
@@ -17,5 +17,34 @@ namespace MathClasses
             m7 = 0; m8 = 0; m9 = 1;
         }
 
+        public Matrix3(float _m1, float _m2, float _m3, float _m4, float _m5, float _m6, float _m7, float _m8, float _m9)
+        {
+            m1 = _m1;
+            m2 = _m2;
+            m3 = _m3;
+            m4 = _m4;
+            m5 = _m5;
+            m6 = _m6;
+            m7 = _m7;
+            m8 = _m8;
+            m9 = _m9;
+        }
+
+        // matrix 3 multiplaction
+        public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
+        {
+            return new Matrix3
+           (lhs.m1 * rhs.m1 + lhs.m2 * rhs.m4 + lhs.m3 * rhs.m7,
+            lhs.m1 * rhs.m2 + lhs.m2 * rhs.m5 + lhs.m3 * rhs.m8,
+            lhs.m1 * rhs.m3 + lhs.m2 * rhs.m6 + lhs.m3 * rhs.m9,
+
+            lhs.m4 * rhs.m1 + lhs.m5 * rhs.m4 + lhs.m6 * rhs.m7,
+            lhs.m4 * rhs.m2 + lhs.m5 * rhs.m5 + lhs.m6 * rhs.m8,
+            lhs.m4 * rhs.m3 + lhs.m5 * rhs.m6 + lhs.m6 * rhs.m9,
+
+            lhs.m7 * rhs.m1 + lhs.m8 * rhs.m4 + lhs.m9 * rhs.m7,
+            lhs.m7 * rhs.m2 + lhs.m8 * rhs.m5 + lhs.m9 * rhs.m8,
+            lhs.m7 * rhs.m3 + lhs.m8 * rhs.m6 + lhs.m9 * rhs.m9);
+        }
     }
 }
--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -54,7 +54,7 @@ namespace MathClasses
             return x * rhs.x + y * rhs.y + z * rhs.z;
         }
 
-        public  Vector3 Cross(Vector3 rhs)
+        public Vector3 Cross(Vector3 rhs)
         {
             return new Vector3
                 (y * rhs.z - z * rhs.y,
@@ -68,6 +68,7 @@ namespace MathClasses
             return (float)Math.Sqrt(x * x + y * y + z * z);
         }
 
+        //vector 3 normalize - Vector3(0, 0, 5) is a direction of 5 units along the z axis, so it has a length of 5. If you normalize that, it's Vector3 becomes (0, 0, 1). You would do this when you only want the direction and don't care about the length.
         public void Normalize()
         {
             float m = Magnitude();
